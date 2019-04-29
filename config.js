8b53d1adbd61a83c38fb7de4fa06f87e8cd17012
juandelaoliva <juandelaoliva1@gmail.com>
2019-04-29 12:27:07 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 //change file name to "config.js"
 module.exports = {
-  botToken: '668491657:AAEH46886Au6yl5pEF5SJOefM6A6i2eW-GY',
+  botToken: 'process.env.botToken',
   adminChatId: 12345678,       //your telegram chat id here, allows usage of /broadcast command
 }
