70456f1466cf02b77e7ff159f3c0881ef77e9955
juandelaoliva <juandelaoliva1@gmail.com>
2019-04-29 11:47:40 +0200

    config

---
+++ config.js
@@ -0,0 +1,5 @@
+//change file name to "config.js"
+module.exports = {
+  botToken: 'process.env.botToken',
+  adminChatId: 12345678,       //your telegram chat id here, allows usage of /broadcast command
+}
